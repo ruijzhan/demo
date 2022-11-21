378f0f7661d75f7530508e2f23200e70c3ee26bf
Ruijian Zhang <qiudog825@gmail.com>
2022-11-21 22:15:39 +0800

    router+group

--- http/framework/core.go
+++ http/framework/core.go
@@ -3,29 +3,70 @@ package framework
 import (
 	"log"
 	"net/http"
+	"strings"
 )
 
 type Core struct {
-	router map[string]ControllerHandler
+	router map[string]map[string]ControllerHandler
 }
 
 func NewCore() *Core {
-	return &Core{router: map[string]ControllerHandler{}}
+
+	return &Core{
+		router: map[string]map[string]ControllerHandler{
+			"GET":    {},
+			"POST":   {},
+			"PUT":    {},
+			"DELETE": {},
+		},
+	}
+}
+
+func (c *Core) addMethod(m, url string, h ControllerHandler) {
+	up := strings.ToUpper(url)
+	c.router[m][up] = h
 }
 
 func (c *Core) Get(url string, handler ControllerHandler) {
-	c.router[url] = handler
+	c.addMethod("GET", url, handler)
+}
+
+func (c *Core) Post(url string, handler ControllerHandler) {
+	c.addMethod("POST", url, handler)
+}
+
+func (c *Core) Put(url string, handler ControllerHandler) {
+	c.addMethod("PUT", url, handler)
+}
+
+func (c *Core) Delete(url string, handler ControllerHandler) {
+	c.addMethod("DELETE", url, handler)
+}
+
+func (c *Core) FindRouteByRequest(r *http.Request) ControllerHandler {
+	m := strings.ToUpper(r.Method)
+	url := strings.ToUpper(r.URL.Path)
+
+	if mm, ok := c.router[m]; ok {
+		if h, ok := mm[url]; ok {
+			return h
+		}
+	}
+	return nil
 }
 
 func (c *Core) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 	log.Println("core.ServeHTTP")
 	ctx := NewContext(req, resp)
 
-	router := c.router["foo"]
-	if router == nil {
+	handler := c.FindRouteByRequest(req)
+	if handler == nil {
+		ctx.Json(404, "not found")
+	}
+
+	if err := handler(ctx); err != nil {
+		ctx.Json(500, "internal error")
 		return
 	}
 
-	log.Println("core.router")
-	router(ctx)
 }
---
+++ http/framework/group.go
@@ -0,0 +1,44 @@
+package framework
+
+type IGroup interface {
+	Get(string, ControllerHandler)
+	Post(string, ControllerHandler)
+	Put(string, ControllerHandler)
+	Delete(string, ControllerHandler)
+}
+
+func (c *Core) Group(prefix string) IGroup {
+	return NewGroup(c, prefix)
+}
+
+type Group struct {
+	core   *Core
+	prefix string
+}
+
+func (g *Group) Get(uri string, h ControllerHandler) {
+	url := g.prefix + uri
+	g.core.Get(url, h)
+}
+
+func (g *Group) Post(uri string, h ControllerHandler) {
+	url := g.prefix + uri
+	g.core.Post(url, h)
+}
+
+func (g *Group) Put(uri string, h ControllerHandler) {
+	url := g.prefix + uri
+	g.core.Put(url, h)
+}
+
+func (g *Group) Delete(uri string, h ControllerHandler) {
+	url := g.prefix + uri
+	g.core.Delete(url, h)
+}
+
+func NewGroup(core *Core, prefix string) *Group {
+	return &Group{
+		core:   core,
+		prefix: prefix,
+	}
+}
