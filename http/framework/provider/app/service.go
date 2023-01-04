358876b9d2a5df1254f481d578e9d65f530935ce
Ruijian Zhang <qiudog825@gmail.com>
2023-01-04 23:52:03 +0800

    add cobra

--- http/framework/provider/app/service.go
+++ http/framework/provider/app/service.go
@@ -35,3 +35,38 @@ func (m MyApp) StorageFolder() string {
 func (m MyApp) LogFolder() string {
 	return filepath.Join(m.baseFolder, "log")
 }
+
+// Version 定义当前版本
+func (m MyApp) Version() string {
+	panic("not implemented") // TODO: Implement
+}
+
+// ConfigFolder 定义了配置文件的路径
+func (m MyApp) ConfigFolder() string {
+	panic("not implemented") // TODO: Implement
+}
+
+// ProviderFolder 定义业务自己的服务提供者地址
+func (m MyApp) ProviderFolder() string {
+	panic("not implemented") // TODO: Implement
+}
+
+// MiddlewareFolder 定义业务自己定义的中间件
+func (m MyApp) MiddlewareFolder() string {
+	panic("not implemented") // TODO: Implement
+}
+
+// CommandFolder 定义业务定义的命令
+func (m MyApp) CommandFolder() string {
+	panic("not implemented") // TODO: Implement
+}
+
+// RuntimeFolder 定义业务的运行中间态信息
+func (m MyApp) RuntimeFolder() string {
+	panic("not implemented") // TODO: Implement
+}
+
+// TestFolder 存放测试所需要的信息
+func (m MyApp) TestFolder() string {
+	panic("not implemented") // TODO: Implement
+}
